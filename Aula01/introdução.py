6d3c8bfa9a3015c124e1441e68c1358c3f40121c
Pedro Paulo <pedropsc100@gmail.com>
2025-05-13 18:44:12 -0300

    Aula01
    Aula01 do dia 06/05 do CtrlYoung1

---
+++ Aula01/introdução.py
@@ -0,0 +1,35 @@
+# print() é o comando utilizado para imprimir algo na tela
+print('Hello World')
+
+# '#' é usada para fazer um comentário de uma linha
+'''
+ as aspas triplas
+ são comentários
+ de múltiplas linhas
+'''
+
+'''
+Na programação é possivel trabalhar com as operações matemáticas com os seguintes simbolos.
++  → Adição
+-  → Subtração
+*  → Multiplicação
+/  → Divisão
+** → Potências
+
+Variáveis → São formas de salvar dados na memória do computador para serem usados em um programa. 
+Os valores salvos podem ser alterados e podem ter tipos diferentes
+
+INT → números inteiros
+FLOAT → números decimais
+STRING → Textos (toda string deve estar entre "" ou '')
+BOOLEANO → Verdadeiro ou Falso (Usado para fazer verificações e controle)
+
+Para declarar uma variável em python basta dar um nome a ela e lhe passar um valor inicial
+Ex.: nota = 10
+
+Regras para declarações de variáveis:
+1ª - O nome deve ser único
+2ª - Nome não pode comaçecar com números
+3ª - Não pode haver espaço nos nomes (contornar da seguinte forma "nome_completo" ou "nomeCompleto")
+4ª - Não pode utilizar caracteres especiais Ex.: ç ~ ^ ? etc
+'''
